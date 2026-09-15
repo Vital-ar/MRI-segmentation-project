aa4418876b8fa93b06cbd1ab83fe9023f87604ee
Vital-ar <vitalii.lavryk.2024@gmail.com>
2026-09-15 22:33:52 +0300

    setting kaggle constants

--- src/constants/kaggle.py
+++ src/constants/kaggle.py
@@ -28,11 +28,11 @@ OPTUNA_DATABASE_URL = 'sqlite:///optuna.db'
 CHECKPOINT_DIR = Path('models', 'checkpoints') 
 
 MODEL_NAME = 'MRI-Unet' # general model name for different versions e.g. MRI-Unet-1 
-NUMBER_OPTUNA_TRIALS = 1  # optuna study parameter #! change this
-BATCH_SIZE = 8 # dataloader parameter #! set to 32
-NUM_WORKERS = 14 # dataloader parameter #!  SET TO NONE
-DEVICES = 1 # trainer parameter  #!  SET TO NONE
-ACCELERATOR = 'cpu' # trainerr parameter  #!  SET TO NONE
+NUMBER_OPTUNA_TRIALS = 40  # optuna study parameter #! change this
+BATCH_SIZE = 32 # dataloader parameter #! set to 32
+NUM_WORKERS = None # dataloader parameter #!  SET TO NONE
+DEVICES = None # trainer parameter  #!  SET TO NONE
+ACCELERATOR = None # trainerr parameter  #!  SET TO NONE
 NUM_EPOCHS = 65  # lightning trainer parameter
 LEARNING_RATE = 0.001 # opitmizer parameter
 WEIGHT_DECAY = 0.01 # optimizer parameter
