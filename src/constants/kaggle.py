f346ecd28b2ef9661d042da3896f4a65a54ae8e2
Vital-ar <vitalii.lavryk.2024@gmail.com>
2026-09-16 14:41:39 +0300

    changing num workers and batch size

--- src/constants/kaggle.py
+++ src/constants/kaggle.py
@@ -28,12 +28,12 @@ OPTUNA_DATABASE_URL = 'sqlite:///optuna.db'
 CHECKPOINT_DIR = Path('models', 'checkpoints') 
 
 MODEL_NAME = 'MRI-Unet' # general model name for different versions e.g. MRI-Unet-1 
-NUMBER_OPTUNA_TRIALS = 13  # optuna study parameter #! change this
-BATCH_SIZE = 8 # dataloader parameter #! set to 32
-NUM_WORKERS = None # dataloader parameter #!  SET TO NONE
+NUMBER_OPTUNA_TRIALS = 40  # optuna study parameter #! change this
+BATCH_SIZE = 4 # dataloader parameter #! set to 32
+NUM_WORKERS = 4 # dataloader parameter #!  SET TO NONE
 DEVICES = None # trainer parameter  #!  SET TO NONE
 ACCELERATOR = None # trainerr parameter  #!  SET TO NONE
-NUM_EPOCHS = 65  # lightning trainer parameter
+NUM_EPOCHS = 13  # lightning trainer parameter
 LEARNING_RATE = 0.001 # opitmizer parameter
 WEIGHT_DECAY = 0.01 # optimizer parameter
 INPUT_CHANNELS = 3 # model input parameter
