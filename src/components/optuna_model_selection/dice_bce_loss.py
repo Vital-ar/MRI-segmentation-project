0449d1d7f9780ac59cc99ebf5eed1251a31c5afb
Vital-ar <vitalii.lavryk.2024@gmail.com>
2026-09-16 18:28:52 +0300

    correcting errors in dice bce loss v2

--- src/components/optuna_model_selection/dice_bce_loss.py
+++ src/components/optuna_model_selection/dice_bce_loss.py
@@ -3,17 +3,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class DiceBCELoss(nn.Module):
-    def init(self, smooth=1.0):
-        super(DiceBCELoss, self).__init__()
+    def __init__(self, smooth=1.0):
+        super().__init__()
         self.smooth = smooth
 
     def forward(self, inputs, targets):
         inputs = torch.sigmoid(inputs)
+
         inputs = inputs.view(-1)
         targets = targets.view(-1)
 
         intersection = (inputs * targets).sum()
-        dice_loss = 1 - (2. * intersection + self.smooth) / (inputs.sum() + targets.sum() + self.smooth)
-        bce = F.binary_cross_entropy(inputs, targets, reduction='mean')
+
+        dice_loss = 1 - (
+            2.0 * intersection + self.smooth
+        ) / (
+            inputs.sum() + targets.sum() + self.smooth
+        )
+
+        bce = F.binary_cross_entropy(
+            inputs,
+            targets,
+            reduction="mean"
+        )
 
         return bce + dice_loss
