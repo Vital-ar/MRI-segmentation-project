a3ccba05ee3e8e17a45e6a3880e4baaf816af223
Vital-ar <vitalii.lavryk.2024@gmail.com>
2026-09-16 17:54:47 +0300

    setting num workers to None

--- src/constants/kaggle.py
+++ src/constants/kaggle.py
@@ -29,8 +29,8 @@ CHECKPOINT_DIR = Path('models', 'checkpoints')
 
 MODEL_NAME = 'MRI-Unet' # general model name for different versions e.g. MRI-Unet-1 
 NUMBER_OPTUNA_TRIALS = 40  # optuna study parameter #! change this
-BATCH_SIZE = 4 # dataloader parameter #! set to 32
-NUM_WORKERS = 4 # dataloader parameter #!  SET TO NONE
+BATCH_SIZE = 16 # dataloader parameter #! set to 32
+NUM_WORKERS = None # dataloader parameter #!  SET TO NONE
 DEVICES = None # trainer parameter  #!  SET TO NONE
 ACCELERATOR = None # trainerr parameter  #!  SET TO NONE
 NUM_EPOCHS = 13  # lightning trainer parameter
