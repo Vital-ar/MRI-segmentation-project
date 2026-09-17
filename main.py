d5887a1e60c57d1711b4d1cbf0d9fa11e852c67c
Vital-ar <vitalii.lavryk.2024@gmail.com>
2026-09-17 10:12:03 +0300

    retrieving checkpoint from crashed run and changing the main function

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
 import os
 from dotenv import load_dotenv
 
-# 1. Load local variables from .env (ignored on Kaggle)
-load_dotenv()
+def main():
 
-# 2. Load cloud variables if running on Kaggle
-if os.environ.get('KAGGLE_KERNEL_RUN_TYPE', None) is not None:
-    from kaggle_secrets import UserSecretsClient
-    user_secrets = UserSecretsClient()
-    print('KAGGLE_KERNEL_RUN_TYPE')
-    os.environ["MLFLOW_TRACKING_URI"] = user_secrets.get_secret("MLFLOW_TRACKING_URI")
-    os.environ["DAGSHUB_USERNAME"] = user_secrets.get_secret("DAGSHUB_USERNAME")
-    os.environ["DAGSHUB_TOKEN"] = user_secrets.get_secret("DAGSHUB_TOKEN")
+    load_dotenv()
 
-    from src.constants.kaggle import RANDOM_STATE
+    if os.environ.get('KAGGLE_KERNEL_RUN_TYPE', None) is not None:
+        from kaggle_secrets import UserSecretsClient
+        user_secrets = UserSecretsClient()
+        os.environ["MLFLOW_TRACKING_URI"] = user_secrets.get_secret("MLFLOW_TRACKING_URI")
+        os.environ["DAGSHUB_USERNAME"] = user_secrets.get_secret("DAGSHUB_USERNAME")
+        os.environ["DAGSHUB_TOKEN"] = user_secrets.get_secret("DAGSHUB_TOKEN")
 
-else:
-    from src.constants.local import RANDOM_STATE
+        from src.constants.kaggle import RANDOM_STATE
 
-
-os.environ["MLFLOW_TRACKING_USERNAME"] = os.environ.get("DAGSHUB_USERNAME")
-os.environ["MLFLOW_TRACKING_PASSWORD"] = os.environ.get("DAGSHUB_TOKEN")
-
-os.environ["AWS_ACCESS_KEY_ID"] = os.environ.get("DAGSHUB_TOKEN")
-os.environ["AWS_SECRET_ACCESS_KEY"] = os.environ.get("DAGSHUB_TOKEN")
+    else:
+        from src.constants.local import RANDOM_STATE
 
 
-from src.pipeline.model_selction_pipe import run_model_selection_pipe
+    os.environ["MLFLOW_TRACKING_USERNAME"] = os.environ.get("DAGSHUB_USERNAME")
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = os.environ.get("DAGSHUB_TOKEN")
 
-import lightning.pytorch as pl
+    os.environ["AWS_ACCESS_KEY_ID"] = os.environ.get("DAGSHUB_TOKEN")
+    os.environ["AWS_SECRET_ACCESS_KEY"] = os.environ.get("DAGSHUB_TOKEN")
 
 
-pl.seed_everything(RANDOM_STATE, workers=True)
+    from src.pipeline.model_selction_pipe import run_model_selection_pipe
 
+    import lightning.pytorch as pl
+
+
+
+    pl.seed_everything(RANDOM_STATE, workers=True)
+
+    run_model_selection_pipe()
 
 if __name__ == '__main__':
 
-    run_model_selection_pipe()
+    main()
--- src/constants/kaggle.py
+++ src/constants/kaggle.py
@@ -29,11 +29,11 @@ CHECKPOINT_DIR = Path('models', 'checkpoints')
 
 MODEL_NAME = 'MRI-Unet' # general model name for different versions e.g. MRI-Unet-1 
 NUMBER_OPTUNA_TRIALS = 40  # optuna study parameter #! change this
-BATCH_SIZE = 16 # dataloader parameter #! set to 32
-NUM_WORKERS = None # dataloader parameter #!  SET TO NONE
+BATCH_SIZE = 4 # dataloader parameter #! set to 32
+NUM_WORKERS = 4 # dataloader parameter #!  SET TO NONE
 DEVICES = None # trainer parameter  #!  SET TO NONE
 ACCELERATOR = None # trainerr parameter  #!  SET TO NONE
-NUM_EPOCHS = 13  # lightning trainer parameter
+NUM_EPOCHS = 5  # lightning trainer parameter
 LEARNING_RATE = 0.001 # opitmizer parameter
 WEIGHT_DECAY = 0.01 # optimizer parameter
 INPUT_CHANNELS = 3 # model input parameter
