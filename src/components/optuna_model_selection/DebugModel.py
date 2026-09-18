c8d1e18f1f936f4ef822623dc8f85d6bd580d2ec
Vital-ar <vitalii.lavryk.2024@gmail.com>
2026-09-18 09:22:31 +0300

    adding gradient required layer to dummy model

--- src/components/optuna_model_selection/DebugModel.py
+++ src/components/optuna_model_selection/DebugModel.py
@@ -5,7 +5,8 @@ import torch
 class Debug(nn.Module):
     def __init__(self):
         super().__init__()
+        self.conv = nn.Conv2d(3, 3, 1)
 
     def forward(self, x):
-        return x
+        return self.conv(x)
     
